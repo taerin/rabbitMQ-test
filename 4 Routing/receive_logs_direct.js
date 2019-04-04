941ede38ff7d285460acce2a0b64000e60b068ac
Taerin Yoon <yooontr@gmail.com>
2019-04-04 18:32:35 +0900

    4 Routing - 선택적 메세지 수신

--- 4 Routing/receive_logs_direct.js
+++ 4 Routing/receive_logs_direct.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+
+//선택적 메세지 수신
 const amqp = require('amqplib/callback_api');
 const args = process.argv.slice(2);
 
@@ -23,6 +25,7 @@ amqp.connect('amqp://localhost', (err, conn) => {
         ch.bindQueue(q.queue, ex, severity);
       });
 
+      // direct교환은 임의의 routingKey를 가질 수 있다.
       ch.consume(q.queue, (msg) => {
         console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
       }, {
